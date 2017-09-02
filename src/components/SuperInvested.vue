9f331401d4aaa123339260a4aacd701fc1352a37
A-A-Z <dramatichamster@gmail.com>
2017-09-02 23:36:37 +1000

    Added drag slider

--- src/components/SuperInvested.vue
+++ src/components/SuperInvested.vue
@@ -5,6 +5,12 @@
     </header>
 
     <h2>How is your super currently invested?</h2>
+    <vue-slider
+      ref="slider"
+      v-model="value"
+      :data="sliderData"
+    ></vue-slider>
+    <h3>{{ value }}</h3>
     <p>
       Your aim is for moderate to higher returns primarily from capital growth
       with some income over the long term. Investments are across all asset types,
@@ -22,12 +28,30 @@
 // to be based on:
 // https://www.amp.com.au/super/supersimulator/#!/super-invested
 
+// using
+// https://github.com/NightCatSama/vue-slider-component
+
+import vueSlider from 'vue-slider-component'
+
 const data = {
-  test: 1
+  value: 'Balanced',
+  sliderData: [
+    'Cash',
+    'Conservative',
+    'Cautious',
+    'Moderately Conservative',
+    'Balanced',
+    'Moderately Aggressive',
+    'Aggressive'
+  ]
 }
 
 export default {
   name: 'super-invested',
+
+  components: {
+    vueSlider
+  },
 
   data: () => {
     return data
